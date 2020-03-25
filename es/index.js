df879031afb3353e3bf881e6af49e0c6ee44fb8a
Robin Weser <contact@weser.io>
2020-03-25 11:56:42 +0100

    Updates

--- es/index.js
+++ es/index.js
@@ -2,6 +2,4 @@ import Absolute from './components/Absolute';
 import Box from './components/Box';
 import ScrollView from './components/ScrollView';
 import Spacer from './components/Spacer';
-import Text from './components/Text';
-import View from './components/View';
-export { Absolute, Box, ScrollView, Spacer, Text, View };
+export { Absolute, Box, ScrollView, Spacer };
